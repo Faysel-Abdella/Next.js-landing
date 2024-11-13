2e4c8af85f47c0eba3930214568eb747c0411264
Faysel Abdella <fayselcode@gmail.com>
2024-11-13 17:18:46 +0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,174 +1,36 @@
-"use client";
-import assets from "@/assets";
-
-import Image from "next/image";
-
-import * as React from "react";
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Button,
-} from "@nextui-org/react";
+'use client';
+import Choices from './Choices';
+import Footer from './Footer';
+import Header from './Header';
+import MainHeader from './MainHeader';
+import assets from '@/assets';
+import Image from 'next/image';
 
 const LandingPage = () => {
-  const [selectedKeys, setSelectedKeys] = React.useState(new Set(["Software"]));
-
-  const selectedValue = React.useMemo(
-    () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
-    [selectedKeys]
-  );
-
   return (
-    <section>
-      <header className="p-10">
-        <div className="flex items-center justify-between">
-          <Image src={assets.logo} alt="logo" width={100} height={100} />
-          <div>
-            <Button
-              className="w-[130px] h-[40px] rounded-[24px] bg-[#003119] font-semibold text-base text-mainWhite leading-[20px] 
-            font-[family-name:var(--font-helvtica-mono)]
-            "
-            >
-              Book a demo
-            </Button>
-          </div>
-        </div>
-      </header>
+    <section className='bg-[#F5F3F0] h-screen flex flex-col justify-between relative'>
+      <Header />
 
-      <main className="mt-20">
-        <div>
-          <div className="flex justify-center items-center">
-            <h3 className="w-fit relative text-7xl tracking-[1px]  leading-[76px] text-center font-bold din">
-              TURN YOUR BORING E-LEARNING <br />
-              INTO AI LIVE INTERACTIVE EXPERIENCES
-              <Image
-                src={assets.star}
-                alt="Start"
-                className="absolute right-[-32px] bottom-[80px]"
-              />
-            </h3>
-          </div>
+      {/* Curved SVG positioned on the left side and centered vertically */}
+      <Image
+        src={assets.leftCurve}
+        alt='Curved SVG'
+        className='absolute left-0 top-1/2 transform -translate-y-1/2'
+      />
 
-          <p className="mt-4 font-medium text-[20px] leading-[30px] text-center text-[#003119] opacity-[60%]">
-            Give your students the feeling of being on a 1-1 Zoom call with
-            their instructors, <br /> significantly improving engagement rates
-            and increasing your revenues.
-          </p>
-        </div>
+      {/* Curved SVG positioned on the right side and centered vertically */}
+      <Image
+        src={assets.RightCurve}
+        alt='Curved SVG'
+        className='absolute right-0 top-1/2 transform -translate-y-1/2'
+      />
 
-        <div className="mt-10 flex justify-center items-center">
-          <div className="flex justify-center items-center gap-[12px] border border-[#EBE9E6] rounded-[40px] py-[4px] px-[20px]">
-            <p className="font-medium text-base leading-[19.54px] text-[#003119] opacity-[60%]">
-              I provide courses on
-            </p>
-
-            <Dropdown>
-              <DropdownTrigger className="bg-[#003119]">
-                <Button
-                  variant="bordered"
-                  className=" bg-opacity-[8%] border border-[#003119] border-opacity-[10%] flex justify-center items-center gap-2"
-                >
-                  <Image src={assets.software} alt="Software" />
-                  <span className="font-semibold text-[#003119] text-base">
-                    {selectedValue}
-                  </span>
-                  <Image src={assets.more} alt="more" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Single selection example"
-                variant="flat"
-                disallowEmptySelection
-                selectionMode="single"
-                selectedKeys={selectedKeys}
-                onSelectionChange={(keys) => {}}
-                classNames={{
-                  base: ["bg-[#003119] rounded-[10px]"],
-                }}
-              >
-                <DropdownItem key="text">
-                  <div className=" bg-opacity-[8%]  flex items-center gap-2">
-                    <Image src={assets.entertainment} alt="Software" />
-                    <span className="font-semibold text-mainWhite opacity-[60%] text-base">
-                      Entertainment
-                    </span>
-                  </div>
-                </DropdownItem>
-                <DropdownItem key="number">
-                  <div className=" bg-opacity-[8%]  flex items-center gap-2">
-                    <Image src={assets.culture} alt="Software" />
-                    <span className="font-semibold text-mainWhite opacity-[60%] text-base">
-                      Culture
-                    </span>
-                  </div>
-                </DropdownItem>
-                <DropdownItem key="date">
-                  <div className=" bg-opacity-[8%]  flex items-center gap-2">
-                    <Image src={assets.maths} alt="Software" />
-                    <span className="font-semibold text-mainWhite opacity-[60%] text-base">
-                      Mathematics
-                    </span>
-                  </div>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-
-            <p className="font-medium text-base leading-[19.54px] text-[#003119] opacity-[60%]">
-              for
-            </p>
-
-            <Dropdown>
-              <DropdownTrigger className="bg-[#003119]">
-                <Button
-                  variant="bordered"
-                  className=" bg-opacity-[8%] border border-[#003119] border-opacity-[10%] flex justify-center items-center gap-2"
-                >
-                  <Image src={assets.people} alt="Software" />
-                  <span className="font-semibold text-[#003119] text-base">
-                    100
-                  </span>
-                  <Image src={assets.more} alt="more" />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Single selection example"
-                variant="flat"
-                disallowEmptySelection
-                selectionMode="single"
-                selectedKeys={selectedKeys}
-                onSelectionChange={(keys) => {}}
-              >
-                <DropdownItem key="text">
-                  <div className=" bg-opacity-[8%]  flex items-center gap-2"></div>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-
-            <p className="font-medium text-base leading-[19.54px] text-[#003119] opacity-[60%]">
-              members
-            </p>
-
-            <button className="flex justify-center items-center size-[40px] rounded-full bg-[#003119]">
-              <Image src={assets.next} alt="Next" />
-            </button>
-          </div>
-        </div>
+      <main className='relative'>
+        <MainHeader />
+        <Choices />
       </main>
 
-      <footer className="p-10">
-        <div className="flex items-center gap-10">
-          <Image src={assets.pr1} alt="Partner one" />
-          <Image src={assets.pr2} alt="Partner two" />
-          <Image src={assets.pr3} alt="Partner three" />
-          <Image src={assets.pr4} alt="Partner four" />
-        </div>
-
-        <p className="mt-[32px]  font-medium leading-[25px] text-[16px] text-[#003119] opacity-[60%]">
-          Trusted by 35,000+ users to engage learners and ensuring
-        </p>
-      </footer>
+      <Footer />
     </section>
   );
 };
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,18 +1,22 @@
-import logo from "./logo.svg";
-import star from "./star.svg";
-import next from "./next.svg";
-import more from "./more.svg";
-import people from "./people.svg";
-import software from "./software.svg";
+import logo from './logo.svg';
+import star from './star.svg';
+import next from './next.svg';
+import more from './more.svg';
+import people from './people.svg';
+import software from './software.svg';
+import starMain from './starMain.svg';
 
-import pr1 from "./pr1.svg";
-import pr2 from "./pr2.svg";
-import pr3 from "./pr3.svg";
-import pr4 from "./pr4.svg";
+import leftCurve from './leftCurve.svg';
+import RightCurve from './rightCurve.svg';
 
-import entertainment from "./entertainment.svg";
-import culture from "./culture.svg";
-import maths from "./maths.svg";
+import pr1 from './pr1.svg';
+import pr2 from './pr2.svg';
+import pr3 from './pr3.svg';
+import pr4 from './pr4.svg';
+
+import entertainment from './entertainment.svg';
+import culture from './culture.svg';
+import maths from './maths.svg';
 
 export default {
   logo,
@@ -21,6 +25,10 @@ export default {
   more,
   people,
   software,
+  starMain,
+
+  leftCurve,
+  RightCurve,
 
   pr1,
   pr2,
